00ffb68201c0bff34d77ed41e1c44b81c01ff4b2
Cyril Hrubis <chrubis@suse.cz>
2017-03-29 18:50:08 +0200

    lib: Fix undefined reference to `mq_open' build failures
    It appears that since the addition of the tst_safe_posix_ipc.c to the
    test library random testcases (mostly ltp-aiodio seems to be triggering
    the issue) started to fail on linking with missing reference to mq_open.
    
    The problem is that -lrt is needed for mq_open() so this commit adds a
    weak stub symbol that is used as fallback when we are compiling without
    -lrt.
    
    Signed-off-by: Cyril Hrubis <chrubis@suse.cz>

--- lib/tst_safe_posix_ipc.c
+++ lib/tst_safe_posix_ipc.c
@@ -5,6 +5,13 @@
 #include "tst_test.h"
 #include "tst_safe_posix_ipc.h"
 
+mqd_t __attribute__((weak)) mq_open(const char *name __attribute__((unused)),
+				    int oflag __attribute__((unused)), ...)
+{
+	tst_brk(TBROK, "mq_open() stub called!");
+	return 0;
+}
+
 int safe_mq_open(const char *file, const int lineno, const char *pathname,
 	int oflags, ...)
 {
