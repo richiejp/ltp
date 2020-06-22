7b62602fde1b53293a0dc77b854317fcd5e79013
Richard Palethorpe <rpalethorpe@suse.com>
2020-06-22 18:07:50 +0100

    Record winner and modify race

--- lib/newlib_tests/tst_fuzzy_sync_demo.c
+++ lib/newlib_tests/tst_fuzzy_sync_demo.c
@@ -20,7 +20,7 @@ static struct tst_option opts[] = {
 
 static struct tst_fzsync_pair pair;
 static FILE *record;
-static volatile char last_wins;
+static volatile char winner;
 
 static long long tons(struct timespec ts)
 {
@@ -31,19 +31,23 @@ static void setup(void)
 {
 	record = SAFE_FOPEN(record_path, "w");
 
-	if (fputs("a_start,b_start,a_end,b_end\n", record) < 0 || fflush(record) != 0)
+	if (fputs("winner,a_start,b_start,a_end,b_end\n", record) < 0 || fflush(record) != 0)
 		tst_brk(TBROK | TERRNO, "Can't write to %s", record_path);
 
 	tst_fzsync_pair_init(&pair);
-	pair.exec_loops = 10000;
+	pair.exec_loops = 100000;
 }
 
 static void *worker(void *v LTP_ATTRIBUTE_UNUSED)
 {
+	volatile unsigned long long c;
+
 	while (tst_fzsync_run_b(&pair)) {
 		tst_fzsync_start_race_b(&pair);
-		usleep(1);
-		last_wins = 'B';
+		c = ~0ULL;
+		while (c)
+			c <<= 1;
+		winner = 'B';
 		tst_fzsync_end_race_b(&pair);
 	}
 
@@ -55,11 +59,14 @@ static void run(void)
 	tst_fzsync_pair_reset(&pair, worker);
 
 	while (tst_fzsync_run_a(&pair)) {
+		winner = 'A';
+
 		tst_fzsync_start_race_a(&pair);
-		last_wins = 'A';
+		if (winner == 'A' && winner == 'B')
+			winner = 'A';
 		tst_fzsync_end_race_a(&pair);
 
-		fprintf(record, "%c,%lld,%lld,%lld,%lld\n", last_wins,
+		fprintf(record, "%c,%lld,%lld,%lld,%lld\n", winner,
 			tons(pair.a_start), tons(pair.b_start),
 			tons(pair.a_end), tons(pair.b_end));
 	}
